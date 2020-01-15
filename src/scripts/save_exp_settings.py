d6804fb15e93397290d0e516c8118d48185ad09b
Duckling92 <bjoernmoelvig@gmail.com>
2020-01-15 23:38:56 +0100

    Moving model to local

--- src/scripts/save_exp_settings.py
+++ src/scripts/save_exp_settings.py
@@ -112,16 +112,16 @@ if __name__ == '__main__':
                         'norm':                {'norm':      'BatchNorm1D', #'BatchNorm1D', 'None'
                                                 'momentum':  0.9 },
 
-                        'layers':              [{'Linear_embedder': {'input_sizes':        [n_seq_feat, 64],
+                        'layers':              [{'Linear_embedder': {'input_sizes':        [n_seq_feat, 128],
                                                                      'LayerNorm':          True},},
-                                                {'SelfAttention':   {'input_sizes':        [64, 64],
+                                                {'SelfAttention':   {'input_sizes':        [128, 128],
                                                                      'LayerNorm':          True,
                                                                      'Residual':           True,}},
                                                 {'MaxPool':          {}},
                                                 #{'LSTM':            {'input_sizes':        [64, 512],
                                                 #                    'dropout':             0.5,
                                                 #                    'bidirectional':       False}},
-                                                {'Linear':          {'input_sizes':        [64+n_scalar_feat, n_target],
+                                                {'Linear':          {'input_sizes':        [128+n_scalar_feat, n_target],
                                                                     'norm_before_nonlin':  True}}]
                         }
                                                 
